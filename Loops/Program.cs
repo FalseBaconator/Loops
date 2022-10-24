bb25fdf2e3b8dab3c7683313150aa55594cebdd0
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-24 12:16:52 -0300

    Made the border and player position visible

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,8 +8,45 @@ namespace Loops
 {
     internal class Program
     {
+        static int storeX = 5;
+        static int storeY = 5;
+
+        static bool gameOver = false;
+
+        static string Input;
+
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            DrawBorder();
+            while(gameOver == false)
+            {
+                PlayerDraw(storeX, storeY);
+                Input = Console.ReadKey(true);
+            }
         }
+
+        static void DrawBorder()
+        {
+            Console.WriteLine("+---------+");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("+---------+");
+        }
+
+        static void PlayerDraw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("O");
+        }
+
+
     }
 }
